a24c45e52f872bfaa912ad7b81145491f2b1aa2d
Taekbeom Yoo <ytbeom@gmail.com>
2022-04-03 02:07:39 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,6 @@
 BUILD_FOLDER="build\libs"
 JAR_NAME="yacht-decision-0.0.1-SNAPSHOT.jar"
 
-if [ ! -d "$BUILD_FOLDER" ] ; then
- gradle bootjar
-fi
-
+gradle bootjar
 cd "$BUILD_FOLDER"
 java -jar "$JAR_NAME"
